45478df77bab31dc920a9fd0c8366f0aa6b0689b
Lorraine <Lorraine@Isaacs-MacBook-Pro.local>
2020-04-12 14:06:03 -0500

    Updates

---
+++ precache-manifest.daa2988541ad4e084ba79e11a07be29a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa8d1e19057ce2fe3a7d6f7ffd9ad9e",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "53fc34cdefc1264a94ba",
+    "url": "/expense-tracker-react/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "9f19646ed2d6d490f7ae",
+    "url": "/expense-tracker-react/static/js/2.76526043.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker-react/static/js/2.76526043.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53fc34cdefc1264a94ba",
+    "url": "/expense-tracker-react/static/js/main.688b614b.chunk.js"
+  },
+  {
+    "revision": "7d48814f4c7a8d57f37a",
+    "url": "/expense-tracker-react/static/js/runtime-main.d766e58e.js"
+  }
+]);
